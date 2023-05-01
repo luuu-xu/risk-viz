c1b0a0ba908807772d973fb60921f0c6895bb50f
luuu-xu <a550763321@gmail.com>
2023-05-01 16:20:37 -0400

    Minor change for deployment

--- src/app/lib/getBaseUrl.ts
+++ src/app/lib/getBaseUrl.ts
@@ -2,6 +2,6 @@ import { cache } from 'react';
 
 export const getBaseUrl = cache(() =>
   process.env.VERCEL_URL
-    ? process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
     : `http://localhost:${process.env.PORT ?? 3033}`,
 );
